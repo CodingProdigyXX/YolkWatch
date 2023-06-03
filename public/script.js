d1df3fe84886b5f0990c5b79bc5c0104057b7013
Ariel Berger <aber7833@uni.sydney.edu.au>
2023-06-03 19:06:16 +1000

    Restting\

--- public/script.js
+++ public/script.js
@@ -1,122 +1,68 @@
-// Setting up variables for our HTML elements using DOM selection
-const form = document.getElementById("taskform");
-const tasklist = document.getElementById("tasklist");
-
-//I have added this comment
-
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    console.log(form.elements.taskType.value)
-
-    addTask(
-        form.elements.taskName.value,
-        form.elements.taskType.value,
-        form.elements.taskRate.value,
-        form.elements.taskTime.value,
-        form.elements.taskClient.value,
-    )
-    console.log(taskList)
-})
-
-function displayTask(task) {
-    let item = document.createElement("li");
-    item.setAttribute("data-id", task.id);
-    item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
-
-    tasklist.appendChild(item);
-
-    // Clear the value of the input once the task has been added to the page
-    form.reset();
-
-    // Setup delete button DOM elements
-    let delButton = document.createElement("button");
-    let delButtonText = document.createTextNode("Delete");
-    delButton.appendChild(delButtonText);
-    item.appendChild(delButton); // Adds a delete button to every task
-
-    // Listen for when the delete button is clicked
-    delButton.addEventListener("click", function (event) {
-
-        taskList.forEach(function (taskArrayElement, taskArrayIndex) {
-            if (taskArrayElement.id == item.getAttribute('data-id')) {
-                taskList.splice(taskArrayIndex, 1)
-            }
-        })
-
-        // Make sure the deletion worked by logging out the whole array
-        console.log(taskList)
-
-        item.remove(); // Remove the task item from the page when button clicked
-        // Because we used 'let' to define the item, this will always delete the right element
-
-    })
-
-
-}
-
-
-
-
-// Create an object called 'task'
-// Populate the properties based on the provided data model
-
-// Commented out now the object creation is included in the function
-
-// var task = {
-//   name: "Initial Sketches",
-//   type: "Concept Ideation",
-//   id: Date.now(),
-//   date: new Date().toISOString(),
-//   rate: 50,
-//   time: 5,
-//   client: "Google"
-// }
-
-// console.log(task);
-
-
-// Create an array called 'taskList'
-var taskList = [];
-
-// Create a function called 'addTask'
-// Give the function input parameters for: name, type, rate, time, client
-// Paste your object definition from above in the function
-// Replace the property values with the input paramaters
-// Add the object to the taskList array
-
-function addTask(name, type, rate, time, client) {
-
-    // Creating the object with the usual property:value syntax
-    // Create task object 
-    // let task = {
-    //   name: name,
-    //   type: type,
-    //   id: Date.now(),
-    //   date: new Date().toISOString(),
-    //   rate: rate,
-    //   time: time,
-    //   client: client
-    // }
-
-    // Creating the object, directly passing in the input parameters
-    let task = {
-        name,
-        type,
-        id: Date.now(),
-        date: new Date().toISOString(),
-        rate,
-        time,
-        client
-    }
-
-    taskList.push(task);
-    displayTask(task);
-
-}
-
-// Call the function with test values for the input paramaters
-addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
-
-// Log the array to the console.
-console.log(taskList);
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Task Tracker</title>
+        <link rel="stylesheet" href="scss/main.scss">
+    </head>
+    <body>
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+        <form id="taskform">
+            <div class="form-row">
+                <label for="taskName">Name</label>
+                <input
+                    type="text"
+                    name="taskName"
+                    id="taskName"
+                    required="true"
+                >
+            </div>
+            <div class="form-row">
+                <label for="taskType">Type</label>
+                <select id="taskType" name="taskType" required="true">
+                    <option value="">Select one</option>
+                    <option value="Concept Ideation">Concept Ideation</option>
+                    <option value="Wireframing">Wireframing</option>
+                    <option value="Application Coding">Application Coding</option>
+                </select>
+            </div>
+            <div class="form-row">
+                <label for="taskRate">Rate</label>
+                <input
+                    type="number"
+                    name="taskRate"
+                    id="taskRate"
+                    required="true"
+                >
+            </div>
+            <div class="form-row">
+                <label for="taskTime">Time</label>
+                <input
+                    type="number"
+                    name="taskTime"
+                    id="taskTime"
+                    required="true"
+                >
+            </div>
+            <div class="form-row">
+                <label for="taskClient">Client</label>
+                <input
+                    type="text"
+                    name="taskClient"
+                    id="taskClient"
+                    required="true"
+                >
+            </div>
+            <div class="form-row">
+                <input type="submit">
+            </div>
+        </form>
+        <h2>Tasks</h2>
+        <ul id="tasklist"></ul>
+        <script src="script.js"></script>
+    </body>
+</html>
