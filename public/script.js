420154a4bd1964cccb36df80e66ba971ca5131c3
Ariel Berger <aber7833@uni.sydney.edu.au>
2023-06-03 19:09:53 +1000

    reset

--- public/script.js
+++ public/script.js
@@ -1,68 +1,99 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Task Tracker</title>
-        <link rel="stylesheet" href="scss/main.scss">
-    </head>
-    <body>
-        <header>
-            <h1>Task Tracker</h1>
-        </header>
-        <form id="taskform">
-            <div class="form-row">
-                <label for="taskName">Name</label>
-                <input
-                    type="text"
-                    name="taskName"
-                    id="taskName"
-                    required="true"
-                >
-            </div>
-            <div class="form-row">
-                <label for="taskType">Type</label>
-                <select id="taskType" name="taskType" required="true">
-                    <option value="">Select one</option>
-                    <option value="Concept Ideation">Concept Ideation</option>
-                    <option value="Wireframing">Wireframing</option>
-                    <option value="Application Coding">Application Coding</option>
-                </select>
-            </div>
-            <div class="form-row">
-                <label for="taskRate">Rate</label>
-                <input
-                    type="number"
-                    name="taskRate"
-                    id="taskRate"
-                    required="true"
-                >
-            </div>
-            <div class="form-row">
-                <label for="taskTime">Time</label>
-                <input
-                    type="number"
-                    name="taskTime"
-                    id="taskTime"
-                    required="true"
-                >
-            </div>
-            <div class="form-row">
-                <label for="taskClient">Client</label>
-                <input
-                    type="text"
-                    name="taskClient"
-                    id="taskClient"
-                    required="true"
-                >
-            </div>
-            <div class="form-row">
-                <input type="submit">
-            </div>
-        </form>
-        <h2>Tasks</h2>
-        <ul id="tasklist"></ul>
-        <script src="script.js"></script>
-    </body>
-</html>
+// Setting up variables for our HTML elements using DOM selection
+const form = document.getElementById("taskform");
+const tasklist = document.getElementById("tasklist");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    addTask(
+        form.elements.taskName.value,
+        form.elements.taskType.value,
+        form.elements.taskRate.value,
+        form.elements.taskTime.value,
+        form.elements.taskClient.value,
+    )
+})
+
+function displayTasks() {
+    
+    tasklist.innerHTML = "";
+    
+    let localTasks = JSON.parse(localStorage.getItem('tasks'));
+    
+    if (localTasks !== null) {
+        
+        localTasks.forEach((task) => {
+            
+            console.log(task)
+        
+            // Create task items for the DOM and add to the list
+            let item = document.createElement("li");
+            item.setAttribute("data-id", task.id);
+            item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
+            tasklist.appendChild(item);
+
+            // Clear the value of the input once the task has been added to the page
+            form.reset();
+
+            // Setup delete button DOM elements
+            let delButton = document.createElement("button");
+            let delButtonText = document.createTextNode("Delete");
+            delButton.appendChild(delButtonText);
+            item.appendChild(delButton); // Adds a delete button to every task
+
+            // Listen for when the delete button is clicked
+            delButton.addEventListener("click", function (event) {
+
+                localTasks.forEach(function (taskArrayElement, taskArrayIndex) {
+                    if (taskArrayElement.id == item.getAttribute('data-id')) {
+                        localTasks.splice(taskArrayIndex, 1)
+                    }
+                })
+                
+                localStorage.setItem('tasks', JSON.stringify(localTasks));
+
+                item.remove(); // Remove the task item from the page when button clicked
+                // Because we used 'let' to define the item, 
+                // this will always delete the right element
+            })
+            
+        }) // Closing brackets for for loop
+    } // Closing bracket for if statement
+}
+
+// Function to add task to the list
+function addTask(name, type, rate, time, client) {
+
+    // Creating the object, directly passing in the input parameters
+    let task = {
+        name,
+        type,
+        id: Date.now(),
+        date: new Date().toISOString(),
+        rate,
+        time,
+        client
+    }
+    
+    // fetching and parse localStorage value
+    let localTasks = JSON.parse(localStorage.getItem('tasks'));
+    
+    if (localTasks == null) {
+        localTasks = [task];
+    } else {
+        // Check to see if there is an existing task
+        if (localTasks.find(element => element.id === task.id)) {
+            console.log('Task ID already exists');
+        } else {
+            localTasks.push(task);
+        }
+    }
+    
+    localStorage.setItem('tasks', JSON.stringify(localTasks))
+
+    displayTasks();
+
+}
+
+// Call the function with test values for the input paramaters
+addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
